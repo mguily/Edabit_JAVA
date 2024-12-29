1a4fdb3ad2a81457074830d42eb162f41f3e887f
Jaime <jaime.martinezd@alumnos.upm.es>
2024-12-29 17:16:20 +0100

    Filtrar transacciones completado.

--- Edabit/src/main/java/org/example/Main.java
+++ Edabit/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Very_Hard.multiplicativePersistence(4));
+        List<Very_Hard.Transaccion> transacciones = Arrays.asList(
+                new Very_Hard.Transaccion(1, 1500, "2023-03"),
+                new Very_Hard.Transaccion(2, 500, "2023-03"),
+                new Very_Hard.Transaccion(3, 2000, "2023-04"),
+                new Very_Hard.Transaccion(4, 1200, "2023-04"),
+                new Very_Hard.Transaccion(5, 2500, "2023-05")
+        );
+        Map<String, Double> resultado = Very_Hard.filtrarTransacciones(transacciones);
+        System.out.println(resultado);
     }
 }
--- Edabit/src/main/java/org/example/Very_Hard.java
+++ Edabit/src/main/java/org/example/Very_Hard.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Very_Hard {
 
@@ -50,6 +51,53 @@ public class Very_Hard {
     }
 
     /*
+    Tienes una lista de transacciones financieras representadas por objetos de la clase Transaction. Cada transacción tiene los siguientes campos:
+
+    id (único identificador de la transacción).
+    amount (monto de la transacción).
+    timestamp (fecha y hora de la transacción).
+    La lista de transacciones es extensa y contiene cientos de miles de elementos.
+
+    Tareas:
+    Filtrar las transacciones: Necesitas obtener las transacciones que tienen un monto superior a 1000 unidades.
+    Agrupar por mes: Posteriormente, debes agrupar las transacciones por el mes y año en que fueron realizadas, con el formato "MM-yyyy".
+    Sumar los montos: Finalmente, para cada grupo (mes/año), debes calcular la suma total de las transacciones.
+
+    Entradas: Una lista de objetos Transaction (de tamaño n).
+    Salida: Un Map<String, Double>, donde cada clave es una cadena con el formato "MM-yyyy" y el valor es la suma total de los
+    montos de las transacciones para ese mes/año.
+
+    Restricciones:
+
+    n es grande, hasta 10^6 elementos en la lista.
+    El rendimiento de la solución es crucial debido a la gran cantidad de datos.
+
+    Ejemplo de Transaccion: Transaccion(1, 1500, "2023-03")
+     */
+
+    public static class Transaccion {
+        private int id;
+        private double amount;
+        private String timestamp;
+
+        public Transaccion(int id, double amount, String timestamp) {
+            this.id = id;
+            this.amount = amount;
+            this.timestamp = timestamp;
+        }
+
+        public int getId() { return id;}
+        public double getAmount() { return amount; }
+        public String getTimestamp() { return timestamp; }
+    }
+
+
+    public static Map<String, Double> filtrarTransacciones(List<Transaccion> lista) {
+        return lista.stream().filter(x -> x.getAmount() > 1000).collect(Collectors.groupingBy(time -> time.getTimestamp().substring(5, 7) + "-" +
+                        time.getTimestamp().substring(0,4), Collectors.summingDouble(Transaccion::getAmount)));
+    }
+
+    /*
     The additive persistence of an integer, n, is the number of times you have to replace n with the sum of its digits until n becomes a single digit integer.
 
     The multiplicative persistence of an integer, n, is the number of times you have to replace n with the product of its digits until n becomes a single digit integer.
@@ -76,4 +124,15 @@ public class Very_Hard {
         return 1 + multiplicativePersistence(nuevoN);
         // Convierto int a un array de chars, parseo cada char a un numero y los multiplico, utilizo recursividad hasta que queda un solo digito para n.
     }
+
+    /*
+    Write a function that takes in three parameters: r, c, i, where:
+
+    r and c are the number of rows and columns to initialize a zero matrix.
+    i represents the array of incrementing operations (+1).
+    And returns the resulting matrix after applying all the increment operations. Each increment operation will add 1
+    to the rows or columns specified in the incrementing array.
+     */
+
+    // Pendiente
 }
