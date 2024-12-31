f9bf7497624a97060ada6e64d13f2d7bd6e142ce
Jaime <jaime.martinezd@alumnos.upm.es>
2024-12-31 15:15:15 +0100

    warOfNumbers completado (Medium).

--- Edabit/src/main/java/org/example/Main.java
+++ Edabit/src/main/java/org/example/Main.java
@@ -1,24 +1,9 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        Object[] cities = {
-                new Object[]{"Madrid", 32230000L},
-                new Object[]{"Barcelona", 1620000L},
-                new Object[]{"Sevilla", 1800000L},
-                new Object[]{"Valencia", 800000L},
-                new Object[]{"Bilbao", 345000L}
-        };
-        Object[] result = Hard.millionsRounding(cities);
-
-        // Mostrar el resultado correctamente
-        for (Object city : result) {
-            // Cast a Object[] para obtener los elementos de cada ciudad
-            Object[] ciudad = (Object[]) city;
-            // Imprimir nombre y población redondeada
-            System.out.println("Ciudad: " + ciudad[0] + ", Población redondeada: " + ciudad[1]);
-        }
+        Medium.warOfNumbers(new int[]{2, 8, 1, 9});
     }
+
+    // Recuento de retos completados: 29 -> 2 Medium, 21 Hard, 6 Very Hard. (31/12/2024).
 }
--- Edabit/src/main/java/org/example/Medium.java
+++ Edabit/src/main/java/org/example/Medium.java
@@ -1,6 +1,26 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
 public class Medium {
+    /*
+    There's a great war between the even and odd numbers. Many numbers already lost their lives in this war and it's your task to end this. You have to determine which group sums larger: the evens, or the odds. The larger group wins.
+
+        Create a function that takes an array of integers, sums the even and odd numbers separately, then returns the difference between the sum of the even and odd numbers.
+
+        Examples
+        warOfNumbers([2, 8, 7, 5]) ➞ 2
+     */
+    public static void warOfNumbers(int[]numbers){
+        int resultado = Arrays.stream(numbers).filter(n -> n % 2 == 0).sum() - Arrays.stream(numbers).filter(n -> n % 2 != 0).sum();
+        if (resultado == 0) System.out.println("Empate");
+        else if (resultado < 0) System.out.println("Ganan los impares"); else System.out.println("Ganan los pares");
+        // Se podria hacer sin usar un Stream pero me propuse hacerlo en el meno numero de lineas posibles.
+        // He cambiado un poco la funcion para determinar ganador.
+    }
+
     /* Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num
     until the array length reaches length.
      */
